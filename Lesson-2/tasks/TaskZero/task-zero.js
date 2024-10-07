91adb48a44cae6fa58e2012e6b721915adf1dbbe
Nataliia Osman <osmannatali22@gmail.com>
2024-10-07 16:41:45 +0200

    Add files via upload

--- Lesson-2/tasks/TaskZero/task-zero.js
+++ Lesson-2/tasks/TaskZero/task-zero.js
@@ -5,12 +5,14 @@ let number2 = 3;
 let number3 = 8;
 
 function calculateExpressions() {
-  const S1 = number1 + 12 + number2;
-  const S2 = Math.sqrt((number1 + number2) / (2 * number1));
-  const S3 = Math.cbrt((number1 + number2) * number3);
-  const S4 = Math.sin(number1 / -number2);
+  const totalSumWithOffset = number1 + 12 + number2;
+  const averageSqrt = Math.sqrt((number1 + number2) / (2 * number1));
+  const cubicRootOfWeightedSum = Math.cbrt((number1 + number2) * number3);
+  const sineOfNegativeRatio = Math.sin(number1 / -number2);
 
-  alert(`S1 = ${S1}\nS2 = ${S2}\nS3 = ${S3}\nS4 = ${S4}`);
+  alert(
+    `S1 = ${totalSumWithOffset}\nS2 = ${averageSqrt}\nS3 = ${cubicRootOfWeightedSum}\nS4 = ${sineOfNegativeRatio}`
+  );
 }
 
 calculateExpressions();
